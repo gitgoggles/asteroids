85bd1350b88307a03acfca005e1f02261213b24a
gitgoggles <ruckojr@hotmail.com>
2025-10-19 18:11:59 +0100

    switched to two groups: drawable and updatable

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for thing in updatable:
+            thing.update(dt)
+        # player.update(dt)
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        # player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
